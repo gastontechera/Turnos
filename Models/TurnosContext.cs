66b0ffe913788610d84fa9e7d39efb8df90df86d
gastontechera <gastontecheram@gmail.com>
2024-06-07 20:12:49 -0300

    Commit

---
+++ Models/TurnosContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Turnos.Models
+{
+    public class TurnosContext : DbContext
+    {
+        
+        public TurnosContext(DbContextOptions<TurnosContext> opciones)
+        : base(opciones)
+        {
+
+        }
+
+        public DbSet<Especialidad> Especialidad { get; set; }
+    }
+}
